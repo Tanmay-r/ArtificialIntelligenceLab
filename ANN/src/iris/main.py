8768d1a26f3755be9db4623e9f970a9c83ba897a
Tanmay-r <tvrandhavane@gmail.com>
2014-03-12 13:54:10 +0530

    added test part to iris

--- ANN/src/iris/main.py
+++ ANN/src/iris/main.py
@@ -23,3 +23,28 @@ backpropagation(layer, inputArray, outputArray, w, eta, M, errorThresh)
 print "Weights: "
 print w
 
+[testInput, testOuput] = generateTrainingData("/home/tanmay/Downloads/GitHub/ArtificialIntelligenceLab/ANN/data/iris/dataset1/test/Iris-setosa"
+	,"/home/tanmay/Downloads/GitHub/ArtificialIntelligenceLab/ANN/data/iris/dataset1/test/Iris-versicolor"
+	,"/home/tanmay/Downloads/GitHub/ArtificialIntelligenceLab/ANN/data/iris/dataset1/test/Iris-virginica")
+
+total = 0
+correct = 0
+for i in testInput.shape[0]:
+	X = testInput[i]
+	actualY = testOuput[i]
+	outputs = feedForward(w, X, layer)
+	Y = outputs[1]
+	Y = Y[::-1]
+	total = total + 1
+	et = 0.05
+	if(computeError(Y, np.array([1,0,0])) < et) :
+		if(actualY[0] == 1):
+			correct = correct + 1
+	if(computeError(Y, np.array([0,1,0])) < et):
+		if(actualY[1] == 1):
+			correct = correct + 1
+	if(computeError(Y, np.array([0,0,1])) < et):
+		if(actualY[2] == 1):
+			correct = correct + 1
+
+print correct, " ", total, " ", correct/(total*1.0)
