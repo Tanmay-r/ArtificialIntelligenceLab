8761d899fc9e1e6ae876944a45856d2aaa817267
alokyadav <yadav.alok29@gmail.com>
2014-03-19 16:37:02 +0530

    new method added to formula class

--- Theorem_Prover/src/formula.cpp
+++ Theorem_Prover/src/formula.cpp
@@ -18,13 +18,13 @@ void formula::print_formula(){
 void formula::put_in_global_list(formula *temp_formula){
 	cout<<"Should not reach here" << endl;
 }
-
 /********************************************************************************/
 binary_formula::binary_formula(formula *temp_lhs , formula * temp_rhs){
 	
 	lhs = temp_lhs;
 	rhs = temp_rhs;
 	formula_status = false ;
+	type = 1;
 }
 
 binary_formula::~binary_formula()
@@ -54,18 +54,37 @@ void binary_formula::print_formula(){
 formula * binary_formula::get_lhs(){
 
 	return lhs;
-	
+
 }
+void binary_formula::make_list(){
+
+	if(hyp_list.size() == 0){
+		hyp_list.push_back(lhs);
+		rhs->make_list();
+		hyp_list.splice(hyp_list.end(), rhs->get_list().begin());
+	}
+}
+list<formula * > binary_formula::get_list(){
+
+	return hyp_list;
+}
+
 void binary_formula::put_in_global_list(formula * temp_formula){
 	
 	Deduction_list.push_back(temp_formula);
 
+}
+int binary_formula::get_type(){
+
+	return type;
 }
 
 /********************************************************************************/
 unary_formula::unary_formula(string var_name){
 	name = var_name;
 	formula_status = false ;
+	type = 0;
+
 }
 
 unary_formula::~unary_formula(){}
@@ -79,7 +98,21 @@ bool unary_formula::get_status(){
 
 	return formula_status ;
 }
+void unary_formula::make_list(){
 
+	if(hyp_list.size() == 0){
+		if(name.compare("F") == 0)
+			hyp_list.push_back(new binary_formula(new unary_formula(name),new unary_formula("F")));
+	}
+}
+list<formula * > binary_formula::get_list(){
+
+	return hyp_list
+}
+
+int unary_formula::get_type(){
+	return type;
+}
 void unary_formula::put_in_global_list(formula * temp_formula){
 
 	Deduction_list.push_back(temp_formula);
--- Theorem_Prover/src/formula.h
+++ Theorem_Prover/src/formula.h
@@ -16,12 +16,17 @@ extern formula * Formula_Object;
 class formula{
 
 	bool formula_status;
+	list<formula *> hyp_list;
+	int type;
 public:
 	formula();
 	~formula();
 	virtual void put_in_global_list(formula * temp_formula);
 	virtual void set_status(bool status);
 	virtual bool get_status();
+	virtual list<formula *> & get_list();
+	virtual void make_list();
+	virtual int get_type();
 	virtual void print_formula();
 
 };
@@ -31,15 +36,18 @@ class binary_formula: public formula
 
 	formula * lhs;
 	formula * rhs;
-	string name;
+	string formula_name;
 
 public:
-	binary_formula(formula *temp_lhs , formula *temp_rhs);
+	binary_formula(formula *temp_lhs , formula *temp_rhs , string name);
 	~binary_formula();
 	void print_formula();
 	void set_status(bool status);
 	bool get_status();
+	list<formula *> & get_list();
 	formula * get_lhs();
+	void make_list();
+	int get_type();
 	void put_in_global_list(formula * temp_formula);
 
 };
@@ -53,7 +61,10 @@ public:
 	void print_formula();
 	void set_status(bool status);
 	bool get_status();
+	list<formula *> & get_list();
 	void put_in_global_list(formula * temp_formula);
+	void make_list();
+	int get_type();
 	string get_variable_name();
 };
 
